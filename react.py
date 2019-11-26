3dba1e3ab52b6a03c5cf75163be6f7d4063dfabb
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2019-11-26 13:12:27 +0100

    CHG blacklist and output improved
    Blacklist: If a token is in the blacklist, we now log it into table
    requests.
    
    CHANGELOG Bei Ergebnissen aus anderen Quellen als 'DS' und 'BOT' wird die Quelle vorangestellt, z.B. 'VGF#HB:'
    CHANGELOG Ergebnisse werden nicht wiederholt, wenn sie einmal mit impliziter Quelle und einmal mit expliziter Quelle geschrieben werden (z.B. '#FF #DS:FF')

--- react.py
+++ react.py
@@ -1,5 +1,6 @@
 import datetime
 import regex as re
+import sqlite3
 
 max_tweet_length = 280
 
@@ -49,12 +50,7 @@ def process_commands(tweet, twapi, verbose):
         if is_followed:
             twapi.defollow(author)
 
-def compose_answer(tweet, sql, verbose, modus):
-    all_answers = []
-    short_list = []
-    # generate answer
-    charcount = 0
-    generated_content = ""
+def find_tokens(tweet, modus):
     finder = re.compile(r"""
                             # this RE finds
                             # #AB:CD
@@ -77,81 +73,102 @@ def compose_answer(tweet, sql, verbose, modus):
         (?:$|\W)            # either end of string or non-\w character
         """, re.X)
     tokens = finder.findall(tweet, overlapped=True)
-    if (modus == 'all'
-        and (
-            len(tokens) == 0
-            or (
-                len(tokens) == 1
-                and tokens[0][0] == '#'
-                and tokens[0][1] == ''
-                and tokens[0][2] == 'DS100'
-            )
-        )):
-        finder2 = re.compile(r"""
-            (?p)            # find longest match
-            (?:^|\W)        # either at the beginning of the text or after a non-alphanumeric character, but don't find this
-            ()
-            ()
-            (
-                [\p{Lu}\p{N}_]+)
-                            # All uppercase letters plus all kinds of numbers plus _
-            (?:$|\W)        # either end of string or non-\w character
-            """, re.X)
-        tokens = finder2.findall(tweet, overlapped=True)
-    for match in tokens:
+    # if modus isn't 'all', then that's all already.
+    if modus != 'all' or len(tokens) > 1:
+        return tokens
+    if len(tokens) == 1 and str.join('', tokens[0][0]) != '#DS100':
+        return tokens
+        
+    finder2 = re.compile(r"""
+        (?p)            # find longest match
+        (?:^|\W)        # either at the beginning of the text or after a non-alphanumeric character, but don't find this
+        ()
+        ()
+        (
+            [\p{Lu}\p{N}_]+)
+                        # All uppercase letters plus all kinds of numbers plus _
+        (?:$|\W)        # either end of string or non-\w character
+        """, re.X)
+    return finder2.findall(tweet, overlapped=True)
+
+def find_entry(sql, parameters):
+    sql.cursor.execute("""
+        SELECT
+            shortstore.Abk AS abk,
+            shortstore.Name AS name,
+            sourceflags.abbr AS source,
+            sourceflags.sigil AS sigil,
+            CASE
+                WHEN (
+                    blacklist.Abk IS NOT NULL
+                    AND
+                    :abbr1 <> :abbr2
+                ) THEN 'blacklist'
+                ELSE 'found'
+            END
+            AS status
+        FROM
+            shortstore
+        JOIN
+            sourceflags
+        ON
+            sourceflags.sourcename = shortstore.source
+        LEFT OUTER JOIN
+            blacklist
+        ON
+            (blacklist.Abk = shortstore.Abk AND blacklist.source = shortstore.source)
+        WHERE
+            shortstore.Abk = :abk
+        AND
+            gueltigvon < strftime('%Y%m%d', 'now')
+        AND
+            sourceflags.sigil = :sigil
+        AND
+            (sourceflags.abbr = :abbr1 OR sourceflags.abbr = :abbr2)
+        ORDER BY gueltigvon DESC
+        LIMIT 1
+        """,
+        parameters
+    )
+    row = sql.cursor.fetchone()
+    normalized = "{}{}:{}".format(parameters['sigil'], parameters['abbr1'], parameters['abk'])
+    if row is None:
+        row = sqlite3.Row(sql.cursor, (
+            parameters['abk'],
+            '',
+            parameters['abbr1'],
+            parameters['sigil'],
+            'notfound'
+        ))
+    return row, normalized
+
+def compose_answer(tweet, sql, verbose, modus):
+    all_answers = []
+    short_list = []
+    # generate answer
+    charcount = 0
+    generated_content = ""
+    for match in find_tokens(tweet, modus):
         sigil = match[0] if not match[0] == "" else '#'
         source = match[1]
         payload = match[2]
         payload = payload.replace('_', ' ')
         payload = ' '.join(payload.split())
         payload = payload.upper()
-        parameters = (payload,
-            sigil,
-            source if source != "" else 'DS',
-            source if source != "" else 'BOT',
-            # if no source has been given, then the search is subject to
-            # blacklisting. (The relevant part is 'NO', the other string
-            # is arbitrary.)
-            'BLACKLIST' if source != "" else 'NO',
-        )
-        sql.cursor.execute("""
-            SELECT
-                shortstore.Abk,
-                shortstore.Name
-            FROM
-                shortstore
-            JOIN
-                sourceflags
-            ON
-                sourceflags.sourcename = shortstore.source
-            LEFT OUTER JOIN
-                blacklist
-            ON
-                (blacklist.Abk = shortstore.Abk AND blacklist.source = shortstore.source)
-            WHERE
-                shortstore.Abk = ?
-            AND
-                gueltigvon < strftime('%Y%m%d', 'now')
-            AND
-                sourceflags.sigil = ?
-            AND
-                (sourceflags.abbr = ? OR sourceflags.abbr = ?)
-            AND
-                (blacklist.source IS NULL OR ? <> 'NO')
-            ORDER BY gueltigvon DESC
-            LIMIT 1
-            """,
-            parameters
-        )
-        row = sql.cursor.fetchone()
-        normalized = '{}{}:{}'.format(sigil, source, payload)
+        parameters = { 'abk': payload,
+            'sigil': sigil,
+            'abbr1': source if source != "" else 'DS',
+            'abbr2': source if source != "" else 'BOT',
+        }
+        row, normalized = find_entry(sql, parameters)
         if verbose > 2:
-            print ("{}: {}→{}".format(normalized, parameters, row))
+            print ("{}: {}→{}".format(normalized,
+            list(parameters.values()), list(row)))
         if normalized in short_list:
             continue
         short_list.append(normalized)
         # failures are always written if the source is not empty, and...
-        if row == None and source == "":
+        if row['status'] == 'notfound' and source == "":
             if len(payload) == 0:
                 continue
             if len(payload) > 5:
@@ -174,11 +191,21 @@ def compose_answer(tweet, sql, verbose, modus):
                 """,
                    (normalized
                   , datetime.datetime.today().strftime('%Y%m%d')
-                  , row != None
+                  , row['status']
                   , ))
-        if row == None:
+        if row['status'] != 'found':
             continue
-        explain = "{}: {}\n".format(row[0], row[1]).replace('\\n', '\n')
+        explain = "{}: {}\n".format(
+            row['abk'],
+            row['name']
+        )
+        if not (row['source'] == 'DS' or row['source'] == 'BOT'):
+            explain = "{}{}{}".format(
+                row['source'],
+                row['sigil'],
+                explain
+            )
+        explain = explain.replace('\\n', '\n')
         if charcount + len(explain) > max_tweet_length:
             all_answers.append(generated_content.strip())
             generated_content = ""
