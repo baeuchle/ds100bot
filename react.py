e64ed5451406ba491198e336a15b1c8cb4de6103
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2020-04-17 21:50:21 +0200

    FIX search for folgenbitte/entfolgen corrected
    Turns out
    for x in ('abc'):
        print(x)
    prints a, b and c, while
    for x in ['abc']:
        print(x)
    prints abc.

--- react.py
+++ react.py
@@ -25,7 +25,7 @@ def process_tweet(tweet, twapi, sql, verbose, magic_tags, modus=None):
 
 def process_commands(tweet, twapi, verbose):
     author = tweet.author()
-    if tweet.has_hashtag(('folgenbitte'), case_sensitive=False):
+    if tweet.has_hashtag(['folgenbitte'], case_sensitive=False):
         is_followed = twapi.is_followed(author)
         if verbose > 0:
             print ("folgenbitte from @{}:".format(author.screen_name), end='')
@@ -35,7 +35,7 @@ def process_commands(tweet, twapi, verbose):
                 print (" not yet following")
         if not is_followed:
             twapi.follow(author)
-    if tweet.has_hashtag(('entfolgen'), case_sensitive=False):
+    if tweet.has_hashtag(['entfolgen'], case_sensitive=False):
         is_followed = twapi.is_followed(author)
         if verbose > 0:
             print ("entfolgen from @{}:".format(author.screen_name), end='')
