f5a4c5b8b02fa95ad31b6cb71ad3f30f91ed9472
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2020-04-19 21:24:11 +0200

    ADD sql scripts for important tables
    CHANGELOG SQL-Script für die wichtigen Meta-Tabellen eingefügt
    
    The tables sources and sourceflags are necessary to make heads or tails
    of the data. Thus, they are included as dumps.

---
+++ config/sources.sql
@@ -0,0 +1,22 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "sources" (
+	`source_name`	TEXT NOT NULL,
+	`abk_col`	TEXT NOT NULL,
+	`name_col`	TEXT NOT NULL,
+	`kurz_col`	TEXT,
+	`valid_from_col`	TEXT,
+	`replace_links`	INTEGER NOT NULL,
+	`delimiter`	TEXT NOT NULL,
+	`valid_until_col`	TEXT,
+	PRIMARY KEY(`source_name`,`source_name`)
+);
+INSERT INTO sources VALUES('ds100','Abk','Name','Kurzname','gültig von',1,';',NULL);
+INSERT INTO sources VALUES('gimmick','Abk','Name',NULL,NULL,0,';',NULL);
+INSERT INTO sources VALUES('strecken','STRNR','STRKURZN','STRNAME',NULL,1,';',NULL);
+INSERT INTO sources VALUES('vgfhst','Abk','Name',NULL,NULL,1,';',NULL);
+INSERT INTO sources VALUES('vgfstrecken','Abk','Weg',NULL,NULL,1,';',NULL);
+INSERT INTO sources VALUES('benannte_strecken','Abk','Beschreibung','Nummer',NULL,1,';',NULL);
+INSERT INTO sources VALUES('db640','Abk','Name',NULL,NULL,1,';',NULL);
+INSERT INTO sources VALUES('leitpunkte','Abk','Name',NULL,NULL,0,';',NULL);
+COMMIT;
