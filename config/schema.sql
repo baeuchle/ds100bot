e251d2c8f5d111f3980ec7e4006fd988abcfbf9a
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2020-07-25 22:52:42 +0200

    FIX SQL Schema whitespace
    pylint score: 10.00
    test score: A 59/0 F 11/0 R 100.0%

--- config/schema.sql
+++ config/schema.sql
@@ -1,31 +1,31 @@
 CREATE TABLE IF NOT EXISTS "last" (
-	`subject`	TEXT NOT NULL,
-	`content`	TEXT
+  `subject` TEXT NOT NULL,
+  `content` TEXT
 );
 CREATE TABLE IF NOT EXISTS "requests" (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`ds100_id`	TEXT NOT NULL,
-	`request_date`	TEXT NOT NULL,
-	`status`	INTEGER
+  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  `ds100_id` TEXT NOT NULL,
+  `request_date` TEXT NOT NULL,
+  `status` INTEGER
 );
 CREATE TABLE IF NOT EXISTS "shortstore" (
-	`id`	TEXT NOT NULL,
-	`Abk`	TEXT NOT NULL,
-	`Name`	TEXT NOT NULL,
-	`Kurzname`	TEXT,
-	`Datenliste`	TEXT,
-	`source`	TEXT,
-	PRIMARY KEY(`id`)
+  `id` TEXT NOT NULL,
+  `Abk` TEXT NOT NULL,
+  `Name` TEXT NOT NULL,
+  `Kurzname` TEXT,
+  `Datenliste` TEXT,
+  `source` TEXT,
+  PRIMARY KEY(`id`)
 );
 CREATE TABLE IF NOT EXISTS "sources" (
-	"source_id"	TEXT NOT NULL,
-	"type"	TEXT,
-	"magic_hashtag"	TEXT,
-	"explicit_source"	TEXT NOT NULL,
-	"is_default"	INTEGER NOT NULL DEFAULT 0 CHECK(is_default in (0,1))
+  "source_id" TEXT NOT NULL,
+  "type" TEXT,
+  "magic_hashtag" TEXT,
+  "explicit_source" TEXT NOT NULL,
+  "is_default" INTEGER NOT NULL DEFAULT 0 CHECK(is_default in (0,1))
 );
 CREATE TABLE IF NOT EXISTS "blacklist" (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`source`	TEXT NOT NULL,
-	`Abk`	TEXT NOT NULL
+  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  `source` TEXT NOT NULL,
+  `Abk` TEXT NOT NULL
 );
