1ecb04b07ff5340af968a266403d96fe2506a3e9
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2020-07-25 22:12:19 +0200

    ADD copyright table generation
    The copyright table is now generated correctly
    
    This fixes #32
    This fixes #52
    
    CHANGELOG Doku: Automatische Erstellung von Lizenz-Übersichtseite https://ds100.frankfurtium.de/copyright.html
    
    pylint score: 10.00
    test score: A 59/0 F 11/0 R 100.0%

--- Documentation/dump.py
+++ Documentation/dump.py
@@ -2,6 +2,14 @@
 
 from xml.etree import ElementTree as ET
 from .generator import Generator
+from .licenses import add_source, add_license
+
+def add_source_line(target, data_obj):
+    add_source(target, data_obj['source'])
+    lic = data_obj.get('license', None)
+    if lic is not None:
+        ET.SubElement(target, 'span').text = ", "
+        add_license(target, lic)
 
 class Dump(Generator):
     def __init__(self, titletext, config, **kwargs):
@@ -20,56 +28,15 @@ class Dump(Generator):
         if self.number_of_data_lists == 1:
             license_p = ET.SubElement(section, 'p')
             license_p.text = 'Quelle: '
-            self.add_license(license_p, config.data_list[0].config)
+            add_source_line(license_p, config.data_list[0].config)
         else:
             ET.SubElement(section, 'p').text = 'Quellen:'
             source_l = ET.SubElement(section, 'ul')
             for dl in config.data_list:
                 li = ET.SubElement(source_l, 'li')
-                li.text = dl.id + ": "
-                self.add_license(li, dl.config)
-
-    @classmethod
-    def add_license(cls, target, data_obj):
-        source = data_obj['source']
-        source_link = source.get('url', None)
-        source_element = ET.SubElement(target, 'span')
-        if source_link is not None:
-            source_element.tag = 'a'
-            source_element.attrib = {'href': source_link}
-        source_element.text = source['name']
-        source_element.tail = ""
-        if source.get('modified', False):
-            source_element.tail += " (modifiziert)"
-        lic = data_obj.get('license', None)
-        if lic is not None:
-            source_element.tail += ", © "
-            lic_link = lic.get('url', None)
-            lic_element = ET.Element('span')
-            if lic_link is not None:
-                lic_element.tag = 'a'
-                lic_element.attrib = {'href': lic_link}
-            target.append(lic_element)
-            lic_element.text = lic['name']
-            owner = lic.get('owner', None)
-            if owner:
-                lic_element.tail = " "
-                owner_type = owner.get('type', None)
-                owner_name = owner.get('name', '')
-                if owner_type == 'twitter':
-                    ET.SubElement(target, 'a', attrib={
-                        'href': 'https://twitter.com/' + owner_name
-                    }).text = "@{} auf twitter".format(owner_name)
-                elif owner_type == 'name':
-                    ET.SubElement(target, 'span').text = owner_name
-                elif owner_type == 'link' and owner.get('url', False):
-                    ET.SubElement(target, 'a', attrib={
-                        'href': owner['url']
-                    }).text = owner_name
-                elif owner_type == 'github':
-                    ET.SubElement(target, 'a', attrib={
-                        'href': 'https://github.com/' + owner_name
-                    }).text = "{} auf github".format(owner_name)
+                ET.SubElement(li, 'strong').text = dl.id
+                ET.SubElement(li, 'span').text = ": "
+                add_source_line(li, dl.config)
 
     def add_row(self, row):
         tr = ET.SubElement(self.table, 'tr')
@@ -82,4 +49,3 @@ class Dump(Generator):
             br.tail = line
         if self.number_of_data_lists > 1:
             ET.SubElement(tr, 'td').text = row[3]
-        # if more than one datalist, add column for which list this belongs to.
--- Documentation/licenses.py
+++ Documentation/licenses.py
@@ -2,6 +2,55 @@
 
 from xml.etree import ElementTree as ET
 from .markdowndoc import MarkdownDoc
+from .generator import Generator
+
+def add_source(target, source):
+    source_link = source.get('url', None)
+    source_element = ET.SubElement(target, 'span')
+    if source_link is not None:
+        source_element.tag = 'a'
+        source_element.attrib = {'href': source_link}
+    source_element.text = source['name']
+    if source.get('modified', False):
+        ET.SubElement(target, 'span').text = " (modifiziert)"
+
+def add_license(target, lic):
+    ET.SubElement(target, 'span').text = "© "
+    lic_link = lic.get('url', None)
+    lic_element = ET.Element('span')
+    if lic_link:
+        lic_element.tag = 'a'
+        lic_element.attrib = {'href': lic_link}
+    target.append(lic_element)
+    lic_element.text = lic['name']
+    owner = lic.get('owner', None)
+    if owner:
+        own = ET.SubElement(target, 'span')
+        own.text = " "
+        owner_type = owner.get('type', None)
+        owner_name = owner.get('name', '')
+        if owner_type == 'name':
+            own.text += owner_name
+        elif owner_type == 'twitter':
+            ET.SubElement(target, 'a', attrib={
+                'href': 'https://twitter.com/' + owner_name
+            }).text = "@{} auf twitter".format(owner_name)
+        elif owner_type == 'github':
+            ET.SubElement(target, 'a', attrib={
+                'href': 'https://github.com/' + owner_name
+            }).text = "{} auf github".format(owner_name)
+        elif owner_type == 'link' and owner.get('url', False):
+            ET.SubElement(target, 'a', attrib={
+                'href': owner['url']
+            }).text = owner_name
+
+def add_comments(target, comments):
+    if len(comments) == 1:
+        target.text = comments[0]
+        return
+    ul = ET.SubElement(target, 'ul')
+    for c in comments:
+        ET.SubElement(ul, 'li').text = c
 
 class Licenses(MarkdownDoc):
     def __init__(self, mdfile, navi):
@@ -16,6 +65,41 @@ class Licenses(MarkdownDoc):
         self.lic = ET.SubElement(lic_table, 'tbody')
 
     def add_source(self, source):
+
         tr = ET.SubElement(self.lic, 'tr')
-        td = ET.SubElement(tr, 'td')
-        td.text = str(source)
+        nodl = len(source.data_list)
+        rowspan = {}
+        if nodl > 1:
+            rowspan = {'rowspan': str(nodl)}
+        ET.SubElement(tr, 'th', attrib=rowspan).text = ', '.join(
+            set('{}{}:'.format(a.type, a.explicit_source) for a in source.access)
+        )
+        if source.desc:
+            ET.SubElement(tr, 'td', attrib=rowspan).append(Generator.xmltext(source.desc))
+        else:
+            ET.SubElement(tr, 'td', attrib=rowspan).text = source.head
+        for dl in source.data_list:
+            add_source(ET.SubElement(tr, 'td'), dl.config['source'])
+            license_container = ET.SubElement(tr, 'td')
+            lic_obj = dl.config.get('license', None)
+            if lic_obj:
+                add_license(license_container, lic_obj)
+            comments_container = ET.SubElement(tr, 'td', attrib={
+                'style': 'text-align: left'
+            })
+            comments = dl.config.get('comments', None)
+            if comments:
+                add_comments(comments_container, comments)
+            if dl == source.data_list[0]:
+                dump_container = ET.SubElement(tr, 'td', attrib=rowspan)
+                ET.SubElement(dump_container, 'a', attrib={
+                    'href': '/dumps/' + source.id + '.html'
+                }).text = source.id
+                mht_container = ET.SubElement(tr, 'td', attrib=rowspan)
+                mht_container.text = ', '.join(
+                    set('#{}'.format(a.magic_hashtag) for a in source.access)
+                )
+                if source.id == 'bot':
+                    mht_container.text = 'N/A'
+            if not dl == source.data_list[-1]:
+                tr = ET.SubElement(self.lic, 'tr')
